c7d322d7bfaefecb5adc35723a059d6fb9f31a60
dhagen.work <dhagen@usc.edu>
2016-07-05 11:37:04 -0700

    started an issue, but realized it should not be implemented to allow for filtering

--- basketball_testing_jun29.py
+++ basketball_testing_jun29.py
@@ -118,6 +118,7 @@ def c_matrix(x1,x2,x3):
 					[	0,				(x3-x2),		2*(x3-x2)	] 	], \
 					float)
 	return(C)
+ipdb.set_trace()
 def y_vector(x1,x2,x3,y1,y2,y3,dyi,dyf):
 	y = np.array([	[	3*(y2-y1)/(x2-x1) - 3*dyi 				],  \
 					[	3*(y3-y2)/(x3-x2) - 3*(y2-y1)/(x2-x1) 	],  \
