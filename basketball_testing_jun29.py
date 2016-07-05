6b64dc690a9601c4f14fa8fe8a911178f91f582f
dhagen.work <dhagen@usc.edu>
2016-07-05 15:22:28 -0700

    Finished adding function descriptions to all functions

--- basketball_testing_jun29.py
+++ basketball_testing_jun29.py
@@ -113,45 +113,94 @@ AngularVelocity3Initial = 0
 AngularVelocity3Final = AngularVelocities[2]
 
 def c_matrix(x1,x2,x3):
+	"""
+	Takes in the values of x1, x2, and x3 to create the C matrix needed to find the coefficients of a clamped
+	cubic spline with only one break (i.e. Cx = y, where x is an array of c coefficients for the
+	piecewise polynomial equation y = a + b*(x-x_o) + c*(x-x_o)**2 + d*(x-x_o)**3). Returns a matrix.
+	"""
 	C = np.matrix([	[	2*(x2-x1), 		(x2-x1), 			0			],   \
 					[	(x2-x1), 		2*(x3-x1), 		(x3-x2)		],   \
 					[	0,				(x3-x2),		2*(x3-x2)	] 	], \
 					float)
 	return(C)
-def y_vector(x1,x2,x3,y1,y2,y3,dyi,dyf):
-	y = np.array([	[	3*(y2-y1)/(x2-x1) - 3*dyi 				],  \
+def y_vector(x1,x2,x3,y1,y2,y3,initial_slope,final_slope):
+	"""
+	Takes in the values of (x1,y1), (x2,y2), and (x3,y3) to create the y array necessary for the clamped cubic
+	spline matrix manipulation for one break only (i.e. Cx = y, where x is an array of c coefficients for the
+	piecewise polynomial equation y = a + b*(x-x_o) + c*(x-x_o)**2 + d*(x-x_o)**3). Returns an array.
+
+	"""
+	y = np.array([	[	3*(y2-y1)/(x2-x1) - 3*initial_slope 				],  \
 					[	3*(y3-y2)/(x3-x2) - 3*(y2-y1)/(x2-x1) 	],  \
-					[	3*dyf - 3*(y3-y2)/(x3-x2) 				] 	],  \
+					[	3*final_slope - 3*(y3-y2)/(x3-x2) 				] 	],  \
 					float)
 	return(y)
-def c_coefficients(x1,x2,x3,y1,y2,y3,dyi,dyf):
+def c_coefficients(x1,x2,x3,y1,y2,y3,initial_slope,final_slope):
+	"""
+	Using matrix manipulations the equation Cx = y necessary for the c coefficients for a clamped cubic spline 
+	with only one break (i.e. Cx = y, where x is an array of c coefficients for the piecewise polynomial 
+	equation y = a + b*(x-x_o) + c*(x-x_o)**2 + d*(x-x_o)**3) can be rearranged such that x = C.T*y. The values
+	(x1,y1), (x2,y2), and (x3,y3) are the three points needed to the spline and initial_slope and final_slope
+	are the endpoint conditions. Returns an array.
+	"""
 	C = c_matrix(x1,x2,x3)
-	y = y_vector(x1,x2,x3,y1,y2,y3,dyi,dyf)
+	y = y_vector(x1,x2,x3,y1,y2,y3,initial_slope,final_slope)
 	CCoefficients = np.dot(inv(C),np.array(y).astype(float))
 	return(np.array(CCoefficients).astype(float))
 def d_coefficients(x1,x2,x3,CCoefficients):
+	"""
+	Uses the c coefficients and the values of x1, x2, and x3 to find the d coefficients for the	piecewise 
+	polynomial equation y = a + b*(x-x_o) + c*(x-x_o)**2 + d*(x-x_o)**3). CCoefficients must be an array with
+	three elements. Returns an array.
+	"""
 	DCoefficients = np.array([	[	(CCoefficients[1]-CCoefficients[0])/(3*(x2-x1))	],  \
 								[	(CCoefficients[2]-CCoefficients[1])/(3*(x3-x2))	] ],  \
 								float)
 	return(DCoefficients)
 def b_coefficients(x1,x2,x3,y1,y2,y3,CCoefficients,DCoefficients):
+	"""
+	Uses the c and d coefficients and the values of (x1,y1), (x2,y2), and (x3,y3) to find the b coefficients for 
+	the	piecewise polynomial equation y = a + b*(x-x_o) + c*(x-x_o)**2 + d*(x-x_o)**3). CCoefficients must be an 
+	array with two or more elements and DCoefficients must be an array with two elements. Returns an array.
+	"""
 	BCoefficients = np.array([	((y2-y1)/(x2-x1)-CCoefficients[0]*(x2-x1) - DCoefficients[0]*((x2-x1)**2)),  \
 								((y3-y2)/(x3-x2)-CCoefficients[1]*(x3-x2) - DCoefficients[1]*((x3-x2)**2)) 	]).astype(float)
 	return(BCoefficients)
-def test_b_coefficients(x1,x2,x3,y1,y2,y3,CCoefficients,DCoefficients,dyi):
-	B = test_b_coefficients(x1,x2,x3,y1,y2,y3,CCoefficients,DCoefficients)
-	assert B[0]==dyi, "First b coefficient (%f) does not equal initial slope (%f)." (B[0],dyi)
+def test_b_coefficients(x1,x2,x3,y1,y2,y3,CCoefficients,DCoefficients,expected_slope):
+	"""
+	Tests to make sure that the generated b coefficients match the expected slope. Uses the c and d coefficients 
+	and the values of (x1,y1), (x2,y2), and (x3,y3) to find the b coefficients for the	piecewise polynomial 
+	equation y = a + b*(x-x_o) + c*(x-x_o)**2 + d*(x-x_o)**3). CCoefficients must be an array with two or more 
+	elements and DCoefficients must be an array with two elements. Returns TRUE if expected_slope equals b.
+	"""
+	B = b_coefficients(x1,x2,x3,y1,y2,y3,CCoefficients,DCoefficients)
+	result = B[0] == expected_slope
+	return(result)
+	assert B[0]==expected_slope, "First b coefficient (%f) does not equal initial slope (%f)." (B[0],expected_slope)
 def a_coefficients(y1,y2):
+	"""
+	Uses the y values of (x1,y1) and (x2,y2) to find the a coefficients for the	piecewise polynomial equation 
+	y = a + b*(x-x_o) + c*(x-x_o)**2 + d*(x-x_o)**3). Returns an array.
+	"""
 	ACoefficients = np.array([	y1,    \
 								y2  ]).astype(float)
 	return(ACoefficients)
-def spline_coefficients(x1,x2,x3,y1,y2,y3,dyi,dyf):
-	C = c_coefficients(x1,x2,x3,y1,y2,y3,dyi,dyf)
+def spline_coefficients(x1,x2,x3,y1,y2,y3,initial_slope,final_slope):
+	"""
+	Uses the values of (x1,y1), (x2,y2), and (x3,y3) to find the coefficients for the piecewise polynomial 
+	equation y = a + b*(x-x_o) + c*(x-x_o)**2 + d*(x-x_o)**3) for a clamped cubic spline with one break only. 
+	Returns coefficient arrays A, B, C,and D.
+	"""
+	C = c_coefficients(x1,x2,x3,y1,y2,y3,initial_slope,final_slope)
 	D = d_coefficients(x1,x2,x3,C)
 	B = b_coefficients(x1,x2,x3,y1,y2,y3,C,D)
 	A = a_coefficients(y1,y2)
 	return(A,B,C,D)
 def generate_random_point(xmin,xmax,ymin,ymax):
+	"""
+	Generates a random point in Cartesian space such that x is in [xmin, xmax] and y is in [ymin,ymax]. 
+	Number is chosen at random from a uniform distribution. Returns two floats.
+	"""
 	x_rand = np.random.uniform(xmin,xmax)
 	y_rand = np.random.uniform(ymin,ymax)
 	return(x_rand,y_rand)
@@ -197,24 +246,30 @@ def is_within_bounds(Spline,ymin,ymax):
 	"""
 	result = max(Spline)<=ymax and min(Spline)>=ymin
 	return(result)
-def clamped_cubic_spline(xi,xf,yi,yf,dyi,dyf,ymin,ymax,X):
+def clamped_cubic_spline(x_initial,x_final,y_initial,y_final,initial_slope,final_slope,ymin,ymax,X):
+	"""
+	This will take in the initial and final values for both x and y, as well as the desired initial and final
+	slopes and generate 1000 clamped cubic spline that produce y values that are within the bounds [ymin, ymax].
+	Returns a list of arrays, each of len(X).
+	"""
 	i = 0
 	while i < 1000:
-		x2,y2 = generate_random_point(xi,xf,ymin,ymax)
-		A,B,C,D = spline_coefficients(xi,x2,xf,yi,y2,yf,dyi,dyf)
-		assert test_endpoint_slope(B[1],C[1],D[1],x2,xf,dyf),"Problem with Endpoint Slope"
-		assert test_for_discontinuity(A[0],B[0],C[0],D[0],xi,x2,A[1]), "Jump Discontinuity at t = %f!" %x2
+		x_rand,y_rand = generate_random_point(x_initial,x_final,ymin,ymax)
+		A,B,C,D = spline_coefficients(x_initial,x_rand,x_final,y_initial,y_rand,y_final,initial_slope,final_slope)
+		assert test_b_coefficients(x_initial,x_rand,x_final,y_initial,y_rand,y_final,C,D,initial_slope)
+		assert test_endpoint_slope(B[1],C[1],D[1],x_rand,x_final,final_slope),"Problem with Endpoint Slope"
+		assert test_for_discontinuity(A[0],B[0],C[0],D[0],x_initial,x_rand,A[1]), "Jump Discontinuity at t = %f!" %x_rand
 		if i == 0:
-			ClampedSpline = piecewise_for_2_intervals(xi,x2,A,B,C,D,X)
+			ClampedSpline = piecewise_for_2_intervals(x_initial,x_rand,A,B,C,D,X)
 			if is_within_bounds(ClampedSpline, ymin, ymax):
 				i+=1
 		elif i == 1:
-			NextClampedSpline = piecewise_for_2_intervals(xi,x2,A,B,C,D,X)
+			NextClampedSpline = piecewise_for_2_intervals(x_initial,x_rand,A,B,C,D,X)
 			if is_within_bounds(NextClampedSpline, ymin, ymax):
 				ClampedSpline = np.concatenate(([ClampedSpline], [NextClampedSpline]), axis = 0)
 				i+=1
 		else:
-			NextClampedSpline = piecewise_for_2_intervals(xi,x2,A,B,C,D,X)
+			NextClampedSpline = piecewise_for_2_intervals(x_initial,x_rand,A,B,C,D,X)
 			if is_within_bounds(NextClampedSpline, ymin, ymax):
 				ClampedSpline = np.concatenate((ClampedSpline, [NextClampedSpline]), axis = 0)
 				i+=1
@@ -228,14 +283,20 @@ Angle3Spline = clamped_cubic_spline(0,EndTime,Angle3Initial,Angle3Final,AngularV
 										AngularVelocity3Final,Angle3Bounds[0],Angle3Bounds[1],Time)
 
 def plot_spline_results(Time,AngleSplines):
+	"""
+	For a 1D vector Time and an N-D list of arrays AngleSplines, this will plot all calculated splines on the
+	same figure. Must close final figure in order to run next line of code.
+	"""
 	plt.figure()
 	for i in range(0,AngleSplines.shape[0]):
 		plt.plot(Time,AngleSplines[i])
 	plt.show()
 
-plot_spline_results(Time,Angle1Spline)
-plot_spline_results(Time,Angle2Spline)
-plot_spline_results(Time,Angle3Spline)
+#plot_spline_results(Time,Angle1Spline)
+#plot_spline_results(Time,Angle2Spline)
+#plot_spline_results(Time,Angle3Spline)
+
+
 
 
 
