03c87a2e56040c18c02d97277bb292c65cf5d21b
dhagen.work <dhagen@usc.edu>
2016-07-05 17:05:29 -0700

    Added moment_arm_matrix() and ran to ensure functionality. Also added slope conditions to clamped_cubic_spline(), closes #5.

--- basketball_testing_jun29.py
+++ basketball_testing_jun29.py
@@ -197,7 +197,7 @@ def test_b_coefficients(x1,x2,x3,y1,y2,y3,CCoefficients,DCoefficients,expected_s
 	elements and DCoefficients must be an array with two elements. Returns TRUE if expected_slope equals b.
 	"""
 	B = b_coefficients(x1,x2,x3,y1,y2,y3,CCoefficients,DCoefficients)
-	result = B[0] == expected_slope
+	result = abs(B[0]-expected_slope)< 0.001
 	return(result)
 	assert B[0]==expected_slope, "First b coefficient (%f) does not equal initial slope (%f)." (B[0],expected_slope)
 def a_coefficients(y1,y2):
@@ -269,37 +269,55 @@ def is_within_bounds(Spline,ymin,ymax):
 	"""
 	result = max(Spline)<=ymax and min(Spline)>=ymin
 	return(result)
-def clamped_cubic_spline(x_initial,x_final,y_initial,y_final,initial_slope,final_slope,ymin,ymax,X):
+def is_slope_positive(x_initial,x_transition,B,C,D,X):
+	"""
+	Takes in the initial x values for the two piecewise polynomials made by the clamped cubic splines with one
+	break and evaluates if the slope is always positive. Returns TRUE if slope is positive for the first 250 ms
+	of a 550 ms shot.
+	"""
+	derivative = np.piecewise(X,[X <= x_transition, X > x_transition], \
+										[lambda X: B[0] + 2*C[0]*(X-x_initial) + 3*D[0]*(X-x_initial)**2, \
+										lambda X: B[1] + 2*C[1]*(X-x_transition) + 3*D[1]*(X-x_transition)**2])
+	result = min(derivative[:2501])>=0
+	return(result)
+def clamped_cubic_spline(x_initial,x_final,y_initial,y_final,initial_slope,final_slope,ymin,ymax,X,**options):
 	"""
 	This will take in the initial and final values for both x and y, as well as the desired initial and final
 	slopes and generate 1000 clamped cubic spline that produce y values that are within the bounds [ymin, ymax].
-	Returns a list of arrays, each of len(X).
+	Returns a list of arrays, each of len(X). Options allows for slope limitations on shoulder rotations such
+	that the derivative of the spline is always positive to match observations (slope = "Shoulder").
 	"""
 	i = 0
 	while i < 1000:
 		x_rand,y_rand = generate_random_point(x_initial,x_final,ymin,ymax)
 		A,B,C,D = spline_coefficients(x_initial,x_rand,x_final,y_initial,y_rand,y_final,initial_slope,final_slope)
-		assert test_b_coefficients(x_initial,x_rand,x_final,y_initial,y_rand,y_final,C,D,initial_slope)
+		assert test_b_coefficients(x_initial,x_rand,x_final,y_initial,y_rand,y_final,C,D,initial_slope), "Initial slope does not match the expected value"
 		assert test_endpoint_slope(B[1],C[1],D[1],x_rand,x_final,final_slope),"Problem with Endpoint Slope"
 		assert test_for_discontinuity(A[0],B[0],C[0],D[0],x_initial,x_rand,A[1]), "Jump Discontinuity at t = %f!" %x_rand
+		if options.get("angle") == "Shoulder":
+			options_slope_condition = is_slope_positive(x_initial,x_final,B,C,D,X)
+		else:
+			options_slope_condition = True
+
 		if i == 0:
 			ClampedSpline = piecewise_for_2_intervals(x_initial,x_rand,A,B,C,D,X)
-			if is_within_bounds(ClampedSpline, ymin, ymax):
+			if is_within_bounds(ClampedSpline, ymin, ymax) and options_slope_condition:
 				i+=1
 		elif i == 1:
 			NextClampedSpline = piecewise_for_2_intervals(x_initial,x_rand,A,B,C,D,X)
-			if is_within_bounds(NextClampedSpline, ymin, ymax):
+			if is_within_bounds(NextClampedSpline, ymin, ymax) and options_slope_condition:
 				ClampedSpline = np.concatenate(([ClampedSpline], [NextClampedSpline]), axis = 0)
 				i+=1
 		else:
 			NextClampedSpline = piecewise_for_2_intervals(x_initial,x_rand,A,B,C,D,X)
-			if is_within_bounds(NextClampedSpline, ymin, ymax):
+			if is_within_bounds(NextClampedSpline, ymin, ymax) and options_slope_condition:
 				ClampedSpline = np.concatenate((ClampedSpline, [NextClampedSpline]), axis = 0)
 				i+=1
 	return(ClampedSpline)
 
 Angle1Spline = clamped_cubic_spline(0,EndTime,Angle1Initial,Angle1Final,AngularVelocity1Initial, \
-										AngularVelocity1Final,Angle1Bounds[0],Angle1Bounds[1],Time)
+										AngularVelocity1Final,Angle1Bounds[0],Angle1Bounds[1],Time,\
+										angle = "Shoulder")
 Angle2Spline = clamped_cubic_spline(0,EndTime,Angle2Initial,Angle2Final,AngularVelocity2Initial, \
 										AngularVelocity2Final,Angle2Bounds[0],Angle2Bounds[1],Time)
 Angle3Spline = clamped_cubic_spline(0,EndTime,Angle3Initial,Angle3Final,AngularVelocity3Initial, \
@@ -314,6 +332,81 @@ def plot_spline_results(Time,AngleSplines):
 	for i in range(0,AngleSplines.shape[0]):
 		plt.plot(Time,AngleSplines[i])
 	plt.show()
+def moment_arm_matrix(Angle1,Angle2,Angle3):
+	#R_tranpose Column 1
+	r1DELTa = 19
+	r1CB = 20
+	r1DELTp = -8
+	r1BIC = 15
+	r1TRI = -15
+	r1BRA = 0
+	r1BRD = 0
+	r1PRO = 0
+	r1FCR = 0
+	r1ECRB = 0
+	r1ECRL = 0
+	r1FCU = 0
+	r1FDS = 0
+	r1PL = 0
+	r1ECU = 0
+	r1EDM = 0
+	r1EDC = 0
+	r1APL = 0
+
+	#R_tranpose Column 2
+	r2DELTa = 0
+	r2CB = 0
+	r2DELTp = 0
+	r2BIC = lambda Angle2: (24777*np.pi)/10000 + (1288265228720957*Angle2)/35184372088832 - (2429*np.pi*Angle2)/125 + (68251*np.pi*(Angle2**2))/5000 - (10427*np.pi*(Angle2**3))/5000 + (20571*(Angle2**2))/10000 - (14043*(Angle2**3))/2500 + 84533/10000
+	r2TRI = lambda Angle2: - (8759*(Angle2**3))/5000 + (93509*(Angle2**2))/10000 - (88691*Angle2)/10000 - 863614486669217/35184372088832
+	r2BRA = lambda Angle2: - (12667*(Angle2**3))/2000 + (30689*(Angle2**2))/1250 - (4544779416463265*Angle2)/281474976710656 + 1139910323808397/70368744177664
+	r2BRD = lambda Angle2: (28129*np.pi)/10000 - (23671*Angle2)/2000 - (57781*np.pi*Angle2)/10000 + (3629*np.pi*(Angle2**2))/1250 - (197*np.pi*(Angle2**3))/500 + (24636921970321*(Angle2**2))/549755813888 - (33739*(Angle2**3))/2500 + 38141/2500
+	r2PRO = lambda Angle2: (3933*np.pi)/10000 - (10079*Angle2)/10000 - (13103*np.pi*Angle2)/1250 + (2597831076304493*np.pi*(Angle2**2))/70368744177664 + (2202*np.pi**2*Angle2)/625 - (93111*np.pi*(Angle2**3))/2500 + (72987*np.pi*(Angle2**4))/5000 - (20089*np.pi*(Angle2**5))/10000 - (4369*np.pi**2)/10000 - (6847666938421497*(Angle2**2))/562949953421312 + (53151*(Angle2**3))/2500 - (5503*(Angle2**4))/500 + (8763*(Angle2**5))/5000 - (1466808324885735*np.pi**2*(Angle2**2))/140737488355328 + (51333*np.pi**2*(Angle2**3))/5000 - (39919*np.pi**2*(Angle2**4))/10000 + (273*np.pi**2*(Angle2**5))/500 + 22081/2000
+	r2FCR = 14
+	r2ECRB = lambda Angle2: (8199*np.pi)/5000 + (44637*Angle2)/2500 - (5073*np.pi*Angle2)/10000 - (471*np.pi*(Angle2**2))/5000 - (28827*(Angle2**2))/10000 - 1407/125
+	r2ECRL = lambda Angle2: (74361*np.pi)/10000 + (72089699777459*Angle2)/4398046511104 - (8783*np.pi*Angle2)/5000 + (371*np.pi**2*Angle2)/5000 - (1667*np.pi**2)/1250 - 38517/5000
+	r2FCU = 19
+	r2FDS = 20
+	r2PL = 25
+	r2ECU = -23
+	r2EDM = -10
+	r2EDC = -20
+	r2APL = 0
+
+	#R_tranpose Column 3
+	r3DELTa = 0
+	r3CB = 0
+	r3DELTp = 0
+	r3BIC = 0
+	r3TRI = 0
+	r3BRA = 0
+	r3BRD = 0
+	r3PRO = 0
+	r3FCR = lambda Angle3: (3199*Angle3)/2000 + 3301/250
+	r3ECRB = lambda Angle3: (21411*Angle3)/10000 - 7562500789275879/562949953421312
+	r3ECRL = lambda Angle3: (457*Angle3)/200 - 58583/5000
+	r3FCU = lambda Angle3: (13307*(Angle3**2))/10000 + (1869*Angle3)/400 + 1578328710658497/140737488355328
+	r3FDS = lambda Angle3: (2099*(Angle3**2))/2000 + (10641*Angle3)/10000 + 5824674283064289/562949953421312
+	r3PL = lambda Angle3: (5011*(Angle3**2))/10000 + (13821*Angle3)/10000 + 3749/400
+	r3ECU = lambda Angle3: (3883*Angle3)/1250 - 21289/2500
+	r3EDM = lambda Angle3: (7603*Angle3)/2500 - 7791/625
+	r3EDC = lambda Angle3: (693*Angle3)/400 - 35319/2500
+	r3APL = lambda Angle3: 1171/2000 - (171*(Angle3**2))/2000 - (73*Angle3)/2500
+
+	R_Matrix = np.matrix([	[r1DELTa, 		r1CB, 			r1DELTp, 		r1BIC, 			r1TRI, 			r1BRA, 	\
+							r1BRD, 			r1PRO, 			r1FCR, 			r1ECRB, 		r1ECRL, 		r1FCU, 	\
+							r1FDS, 			r1PL,   		r1ECU, 			r1EDM, 			r1EDC, 			r1APL], \
+							[r2DELTa, 		r2CB, 			r2DELTp, 		r2BIC(Angle2), 	r2TRI(Angle2), 	r2BRA(Angle2),  \
+							r2BRD(Angle2), 	r2PRO(Angle2), 	r2FCR,			r2ECRB(Angle2), r2ECRL(Angle2), r2FCU,  \
+							r2FDS, 			r2PL, 			r2ECU, 			r2EDM, 			r2EDC, 			r2APL], \
+							[r3DELTa, 		r3CB, 			r3DELTp, 		r3BIC, 			r3TRI, 			r3BRA, \
+							r3BRD, 			r3PRO, 			r3FCR(Angle3), 	r3ECRB(Angle3), r3ECRL(Angle3), r3FCU(Angle3), \
+							r3FDS(Angle3), 	r3PL(Angle3), 	r3ECU(Angle3), 	r3EDM(Angle3), 	r3EDC(Angle3), 	r3APL(Angle3)]	], \
+							float)
+	return(R_Matrix)
+
+	print(moment_arm_matrix(0,0,0))
+
 
 #plot_spline_results(Time,Angle1Spline)
 #plot_spline_results(Time,Angle2Spline)
